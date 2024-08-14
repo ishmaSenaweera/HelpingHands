80d92f5c7a905d563ff9c74d916d7a0fca8dff75
ishmaSenaweera <ishmasenaweera@example.com>
2024-08-14 06:00:09 +0530

    First Commit

---
+++ lib/screens/Authentication/widget_tree.dart
@@ -0,0 +1,32 @@
+import 'package:project/screens/Authentication/auth.dart';
+import 'package:project/screens/Authentication/home_page.dart';
+import 'package:project/screens/Authentication/login_register_page.dart';
+import 'package:project/screens/Authentication/email_verification_page.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData && snapshot.data!.emailVerified) {
+          return HomePage();
+        }
+        // else if (snapshot.hasData && !snapshot.data!.emailVerified) {
+        //   return const EmailVerificationPage();
+        // }
+        else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
